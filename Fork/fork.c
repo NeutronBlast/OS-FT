3cf817725d227981c75b11179bc5a05b1b6adf94
metalsonic21 <45906155+metalsonic21@users.noreply.github.com>
2019-05-28 21:23:34 -0400

    I almost fuck shit up

---
+++ Fork/fork.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int op = -1, n = 0, g = 0;
+	while (1 == 1) {
+		system("clear");
+		printf("\n\n        Menu Principal Proyecto\n\n");
+		printf("    1. Crear vector por medio de archivo\n");
+		printf("    2. Ordenar vector \n");
+			printf("    0. Salir\n\n Marque opcion =>");
+		scanf("%i", &op);
+
+		switch (op) {
+		case 1: 
+			break;
+		case 2: 
+			break;
+		case 0: break;
+		default:
+			printf("Opcion NO Valida ... intente de nuevo");
+			break;
+		}
+		if (!(op)) 
+		break;
+	}
+}
