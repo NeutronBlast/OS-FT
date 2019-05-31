2c68f40f3eab02245f0e909a19fc309289efd74b
metalsonic21 <45906155+metalsonic21@users.noreply.github.com>
2019-05-30 23:01:08 -0400

    BINARY FILE WORKS

--- Fork/manejoArchivos.h
+++ Fork/manejoArchivos.h
@@ -1,17 +1,15 @@
 void llenarVector(FILE *e, int vector [], int n) {
-	int i = 0, l = 0;
-	char placeholder[15];
-	char *linea;
-	unsigned long int fileLen;
+	for (int i=0; i<n; i++){
+		fscanf(e, "%d", &vector[i]);
+		printf("value %d\n", vector[i]);
+	}
 
-
-
-
+ fclose(e);
 }
 
 void abrirArchivoEntrada (char * nombre, int vector[], int n){
     FILE *e;
-    e = fopen(nombre, "rb");
+    e = fopen(nombre, "rb+");
 	    if (e == NULL){
 		/*Si hay errores en el archivo*/
 			    printf("Archivo de enteros desordenados esta vacio o no existe\n");
